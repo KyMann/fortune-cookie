38912f1a693c692e2a4641703fe6b6fd30b3fd13
Kyle Mann <akavir7@gmail.com>
2017-01-23 22:12:18 -0600

    implemented fortune, number and refresh button

--- main.py
+++ main.py
@@ -16,19 +16,48 @@
 #
 import webapp2
 import random
+def getRandomFortune():
+    #select from a list of fortunes
+    fortunes = [
+    "I see much code in your future",
+    "Consider eating more fortune cookies",
+    "You have tamed the mighty Python, now you must free it onto the Great Spider's Web!",
+    "You would be great as a TI",
+    "A feather in the hand is better than a bird in the air",
+    "A friend asks only for your time not your money.",
+    "A new perspective will come with the new year",
+    ]
+    #randomley select a fortune
+    index = random.randint(0, len(fortunes)-1)
+    return(fortunes[index])
 
 class MainHandler(webapp2.RequestHandler):
+    '''home page
+    '''
     def get(self):
-        lucky_number = random.randint(1, 100)
         header = "<h1>Fortune Cookie</h1>"
-        number_sentence ='Your lucky number: ' + str(lucky_number) 
-        number_paragraph = "<p>" + number_sentence + "</p>"
-        self.response.write(header + number_paragraph)
-
-class LoginHandler(webapp2.RequestHandler):
-        def 
         
-app = webapp2.WSGIApplication([
+        fortune = "<strong>" +getRandomFortune() +"</strong>"
+        fortune_sentence = "Your fortune is: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+        
+        lucky_number = "<strong>" + str(random.randint(1,100)) +"</strong>"
+        number_sentence ='Your lucky number: ' + (lucky_number) 
+        number_paragraph = "<p>" + number_sentence + "</p>"
+        
+        cookie_again_button = "<a href='.'><button>Another Cookie Plz!</button></a>"
+        
+        content = header + fortune_paragraph +number_paragraph +cookie_again_button
+        self.response.write(content)
+
+class LoginHandler(webapp2.RequestHandler): #why bother deleting a good example?
+    '''login page
+    '''
+    def get(self):
+        self.response.write("Thanks for trying to log in!")
+        
+app = webapp2.WSGIApplication([ #app is an instance of this premade class that takes in routes as arguments
     ('/', MainHandler),
-    ('/login', MainHandler)
+    ('/login', LoginHandler) #it's not hurting anything by staying!
 ], debug=True)
+ 
